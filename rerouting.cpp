f98b1327a78c020c8c8ef232c4306ec23362ae30
kt117 <divinediscon10t@gmail.com>
2020-03-31 04:33:02 +0900

    change: vectorのが使いやすそうなので変更

--- rerouting.cpp
+++ rerouting.cpp
@@ -11,40 +11,19 @@ const ll INF = 1e18;
 #define print(x) cout << (x) << endl;
 #define printa(x,m,n) for(ll i = (m); i <= n; i++){cout << (x[i]) << " ";} cout<<endl;
 
-const long long MAX = 2e5+100;
-long long fac[MAX], finv[MAX], inv[MAX];
-
-void COMinit() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < MAX; i++){
-        fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
-        finv[i] = finv[i - 1] * inv[i] % MOD;
-    }
-}
-
-long long COM(int n, int k){
-    if (n < k) return 0;
-    if (n < 0 || k < 0) return 0;
-    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
-}
-
 ll n;
-vector<vector<ll>> g;
-P dp[MAX];
+vector<vector<ll>> g, dp;
 
-P ide(){
-    return P(0, 1);
+vector<ll> ide(){
+    return {1, 0};
 }
 
-P f1(P a, P b){
-    return P(a.first+b.first, (a.second*b.second%MOD)*COM(a.first+b.first, b.first)%MOD);
+vector<ll> f1(vector<ll> a, vector<ll> b){
+    return {a[0]*b[0]%MOD, (a[0]*b[1]%MOD+a[1]*b[0]%MOD+a[1]*b[1]%MOD)%MOD};
 }
 
-P f2(P a){
-    return P(a.first+1, a.second);
+vector<ll> f2(vector<ll> a){
+    return {(a[0]+a[1])%MOD, a[0]};
 }
 
 void dfs1(ll at, ll from = -1){
@@ -58,11 +37,11 @@ void dfs1(ll at, ll from = -1){
     }
 }
 
-void dfs2(ll at, ll from = -1, P x = P(0, 1)){
+void dfs2(ll at, ll from = -1, vector<ll> x = ide()){
     dp[at] = f2(f1(dp[at], x));
 
     ll m = g[at].size();
-    P l[m], r[m];
+    vector<ll> l[m], r[m];
     zep(i, 0, m){l[i] = r[i] = (g[at][i] == from)? x : f2(dp[g[at][i]]);}
     rep(i, 1, m-1){l[i] = f1(l[i], l[i-1]);}
     rrep(i, m-2, 0){r[i] = f1(r[i], r[i+1]);}
@@ -70,7 +49,7 @@ void dfs2(ll at, ll from = -1, P x = P(0, 1)){
     zep(i, 0, m){
         ll nx = g[at][i];
         if(nx != from){
-            P tmp = ide();
+            vector<ll> tmp = ide();
             if(i > 0){tmp = f1(tmp, l[i-1]);}
             if(i < m-1){tmp = f1(tmp, r[i+1]);}
             dfs2(nx, at, f2(tmp));
@@ -80,11 +59,10 @@ void dfs2(ll at, ll from = -1, P x = P(0, 1)){
 
 int main(){
     cin.tie(0); ios::sync_with_stdio(false);
-    
-    COMinit();
 
     cin >> n;
     g.resize(n);
+    dp.resize(n);
     zep(i, 0, n-1){
         ll a, b;
         cin >> a >> b;
@@ -93,9 +71,8 @@ int main(){
         g[b].push_back(a);
     }
 
-    dfs1(0);    
+    dfs1(0);
     dfs2(0);
-
-    zep(i, 0, n){print(dp[i].second)}
+    print((dp[0][0]+dp[0][1])%MOD)
     return 0;
 }
