b6bad7c7277227adce751a06229400f4f912ca37
kt117 <divinediscon10t@gmail.com>
2020-03-31 00:42:07 +0900

    init: union find

---
+++ union_find.cpp
@@ -0,0 +1,34 @@
+struct union_find{
+	vector<int> rnk, par;
+	
+	union_find(int n) {
+		rnk.resize(n);
+		par.resize(n);
+		for(int i = 0; i < n; i++){
+			par[i] = i;
+			rnk[i] = 0;
+		}
+	}
+	
+	int find(int x) {
+		if (par[x] == x) return x;
+		return par[x] = find(par[x]);
+	}
+ 
+	void unite(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x == y)return;
+		if (rnk[x] < rnk[y]) {
+			par[x] = y;
+		}
+		else {
+			par[y] = x;
+			if (rnk[x] == rnk[y])rnk[x]++;
+		}
+	}
+	
+	bool same(int x, int y) {
+		return find(x) == find(y);
+	}
+};
